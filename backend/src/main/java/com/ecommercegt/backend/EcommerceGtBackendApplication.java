ff71db400c3d3610941a08593023c112e9ea7888
David Cano <davidcano202132346@cunoc.edu.gt>
2025-10-21 10:29:27 -0600

    agrgando dependencias

---
+++ backend/src/main/java/com/ecommercegt/backend/EcommerceGtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercegt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceGtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceGtBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ecommercegt/backend/EcommerceGtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercegt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceGtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
